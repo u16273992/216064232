12008766bac8003eaed5044c245f181a018010bd
u16273992 <u16273992@tuks.co.za>
2022-11-17 22:35:06 +0200

    added categories on blogs

--- myBlogProject/myBlog/forms.py
+++ myBlogProject/myBlog/forms.py
@@ -1,16 +1,18 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
 
+choices = Category.objects.all().values_list('name','name')
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category','body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class':'form-control'}),
             'title_tag': forms.TextInput(attrs={'class':'form-control'}),
             'author': forms.Select(attrs={'class':'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class':'form-control'}),
             'body': forms.Textarea(attrs={'class':'form-control'}),
 
         }
--- myBlogProject/myBlog/views.py
+++ myBlogProject/myBlog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Post
+from .models import Post, Category
 from .forms import PostForm, EditForm
 from django.urls import reverse_lazy
 
@@ -19,6 +19,11 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
+
+class AddCategoryView(CreateView):
+    model = Category
+    template_name = 'add_category.html'
+    fields = "__all__"
 
 class UpdatePostView(UpdateView):
     model = Post
