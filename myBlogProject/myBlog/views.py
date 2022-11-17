058f1e0951d2e85ee82ae1947c8d2b51022e8a0c
u16273992 <u16273992@tuks.co.za>
2022-11-17 19:01:53 +0200

    delete post functionality added

--- myBlogProject/myBlog/views.py
+++ myBlogProject/myBlog/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-from .forms import PostForm
-
-
-
-#def home(request):
-#    return render(request, 'home.html',{})
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 
 class ArticleDetailView(DetailView):
@@ -22,3 +19,13 @@ class AddPostView(CreateView):
     model = Post
     form_class = PostForm
     template_name = 'add_post.html'
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url= reverse_lazy('home')
