6c598e9772af58b8693c381937eec6a8b58cfeef
u16273992 <u16273992@tuks.co.za>
2022-11-18 20:18:45 +0200

    user restrictions to edit added

--- myBlogProject/myBlog/views.py
+++ myBlogProject/myBlog/views.py
@@ -34,3 +34,7 @@ class DeletePostView(DeleteView):
     model = Post
     template_name = 'delete_post.html'
     success_url= reverse_lazy('home')
+
+def CategoryView(request, cats):
+    category_posts = Post.objects.filter(category = cats.replace('-',' '))
+    return render(request, 'categories.html', {'cats':cats.title().replace('-',' '), 'category_posts':category_posts})
